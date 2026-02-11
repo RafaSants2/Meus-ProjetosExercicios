ab1627e41b2c331a91ddc08e0b797263012da70b
RafaelSantana2007 <vrauzinmr2000@gmail.com>
2026-02-11 20:25:46 -0300

    .

--- PROJETO-CLIMÁTICO/script.js
+++ PROJETO-CLIMÁTICO/script.js
@@ -1,10 +1,13 @@
 const horario = document.getElementById("horario");
+const grausC = document.getElementById("graus");
 
 setInterval(() => {
   const agora = new Date();
   horario.textContent = agora.toLocaleTimeString("pt-BR", {
     hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit"
+    minute: "2-digit"
   });
 }, 1000);
+
+setInterval(() => {
+}, 3600000)
